ed7f79a786b895ee98b15d633f41855915627cab
leeb5737 <brl013@ucsd.edu>
2022-02-11 13:28:52 -0800

    Doesnt work yet

--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
-set -e
-test: MarkdownParse.java MarkdownParseTest.java
+test: MarkdownParse.java MarkdownParseTest.java 
 	javac MarkdownParse.java
 	javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java 
 	java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 set -e
-javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java 
-java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar GradingTests.java 
+java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore GradingTests
